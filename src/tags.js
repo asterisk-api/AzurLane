41f156eccdc53a5dd1fbb333b207d925165f448d
Fernando <81207411+Fernando2603@users.noreply.github.com>
2022-07-27 04:49:22 +0000

    Data Update

--- src/tags.js
+++ src/tags.js
@@ -13,14 +13,18 @@ export default function tags(azurapi)
 	{
 		const TAGS = [];
 
-		const SHIP_ID = ship.id;
-		const NAME 	  = ship.names.en;
+		const SHIP_ID  = ship.id;
+		const NAME     = ship.names.en;
+		const CODENAME = ship.names.code;
 
 		const RARITY   = ship.rarity;
+		const NATION   = ship.nationality;
+		const HULLTYPE = ship.hullType;
 		const RETROFIT = ship.retrofit;
+		const RETROFIT_HULLTYPE = ship.retrofitHullType;
 
 		// Research
-		if (["Priority", "Decisive"].includes(RARITY));
+		if (["Priority", "Decisive"].includes(RARITY))
 			TAGS.push("Research");
 
 		// Retrofit
@@ -32,6 +36,51 @@ export default function tags(azurapi)
 		if (RARITY_LIST.includes(RARITY))
 			TAGS.push(RARITY);
 		else console.log("=> rarity not match: " + RARITY);
+
+		// Muse
+		if (NAME.includes("μ"))
+			TAGS.push("Muse");
+
+		// Bulin
+		if (["001", "002", "003"].includes(SHIP_ID))
+			TAGS.push("Bulin");
+
+		// NATIONALITY
+		if (NATION) TAGS.push(NATION);
+		else console.log("=> invalid nationality: " + NATION);
+
+		// PREFIX
+		const PREFIX = [
+			"UNIV",     // Universal
+			"USS",      // Eagle Union
+			"HMS",      // Royal Navy
+			"IJN",      // Sakura Empire
+			"KMS",      // Iron Blood
+			"SMS",      // Iron Blood
+			"PRAN",     // Dragon Empery
+			"ROC",      // Dragon Empery
+			"RN",       // Sardegna Empire
+			"SN",       // Northern Parliament
+			"FFNF",     // Iris Libre
+			"MNF",      // Vichya Dominion
+			"BILI",     // Bilibili
+			"KizunaAI", // KizunaAI
+			"HDN",      // Neptunia
+		];
+		if (PREFIX.includes(CODENAME.split(" ")[0]))
+			TAGS.push(CODENAME.split(" ")[0]);
+		else if (CODENAME.includes("KizunaAI"))
+			TAGS.push("KizunaAI");
+
+		// Hull Type
+		if (HULLTYPE)
+			TAGS.push(HULLTYPE);
+		else console.log("invalid hullType: " + HULLTYPE);
+
+		// Retrofit Hull Type
+		if (RETROFIT_HULLTYPE && RETROFIT_HULLTYPE !== HULLTYPE)
+			TAGS.push(RETROFIT_HULLTYPE);
+
 
 		CONTAINER.push({
 			id: SHIP_ID,
